0abb7c99010e35c23afddb8853e8176f20d049b6
sandybansalhere <199185188+sandybansalhere@users.noreply.github.com>
2025-06-20 02:09:42 +0530

    negative number count

--- easy/02-negative-number-count/negative-number-count.test.ts
+++ easy/02-negative-number-count/negative-number-count.test.ts
@@ -4,14 +4,14 @@ let arr1 = [1, 4, -5, 8, -9, 0];
 let arr2 = [1, 4, 8, 8, -9, 0];
 let arr3 = [1, 4, 8, 8, 10, 0];
 
-test("should return 2", () => {
+test("should return 2 for arr1", () => {
   expect(negativeNumberCount(arr1)).toBe(2);
 });
 
-test("should return 1", () => {
+test("should return 1 for arr2", () => {
   expect(negativeNumberCount(arr2)).toBe(1);
 });
 
-test("should return 0", () => {
+test("should return 0 for arr3", () => {
   expect(negativeNumberCount(arr3)).toBe(0);
 });
