c98425d084da0acfc53550515b99c79249e87db9
sandybansalhere <199185188+sandybansalhere@users.noreply.github.com>
2025-06-20 01:58:28 +0530

    largest number

---
+++ easy/03-largest-number/largest-number.test.ts
@@ -0,0 +1,17 @@
+import { largestNumber } from "./largest-number";
+
+let arr1 = [2, 4, 7, 9, 19, 3, 1];
+let arr2 = [9, 1, 30, 8, 19, 0, 5];
+let arr3 = [7, 1, 8, 2, 0, 4, 1, 5];
+
+test("should return 19 for arr1", () => {
+  expect(largestNumber(arr1)).toBe(19);
+});
+
+test("should return 30 for arr2", () => {
+  expect(largestNumber(arr2)).toBe(30);
+});
+
+test("should return 30 for arr3", () => {
+  expect(largestNumber(arr3)).toBe(8);
+});
---
+++ easy/03-largest-number/largest-number.ts
@@ -0,0 +1,15 @@
+/**
+ * Write a function that returns the largest number from the array
+ *
+ * Example: 1
+ * Input: arr = [3, 8, 9, 11, 2, 1, 16, 5]
+ * Output: 16
+ *
+ * Example: 2
+ * Input: arr = [3, 18, 9, 19, 2, 1, 16, 15]
+ * Output: 19
+ */
+
+export function largestNumber(arr: number[]) {
+  // TODO
+}
