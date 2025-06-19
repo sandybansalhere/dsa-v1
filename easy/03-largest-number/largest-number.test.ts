cef74f4fbd5c1bc3eac85342de3de8e7f517a543
sandybansalhere <199185188+sandybansalhere@users.noreply.github.com>
2025-06-20 02:09:26 +0530

    largest number count

--- easy/03-largest-number/largest-number.test.ts
+++ easy/03-largest-number/largest-number.test.ts
@@ -1,8 +1,9 @@
-import { largestNumber } from "./largest-number";
+import { largestNumber } from "./solution";
 
 let arr1 = [2, 4, 7, 9, 19, 3, 1];
 let arr2 = [9, 1, 30, 8, 19, 0, 5];
 let arr3 = [7, 1, 8, 2, 0, 4, 1, 5];
+let arr4 = [-9, -5, -2];
 
 test("should return 19 for arr1", () => {
   expect(largestNumber(arr1)).toBe(19);
@@ -14,4 +15,8 @@ test("should return 30 for arr2", () => {
 
 test("should return 8 for arr3", () => {
   expect(largestNumber(arr3)).toBe(8);
+});
+
+test("should return -2 for arr4", () => {
+  expect(largestNumber(arr4)).toBe(-2);
 });
